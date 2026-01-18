c6abd07c0cad0d415ba50acce676dceb0adabfa5
matiusfebrianchristo <chriss.tkj@gmail.com>
2026-01-18 18:51:14 +0700

    Tugas live session 2

---
+++ app/(landing)/components/checkout/cart-items.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import priceFormatter from "@/app/utils/price-formatter";
+import {cartList} from "../ui/cart-popup";
+import Image from "next/image";
+import Button from "../ui/button";
+import { FiArrowRight, FiCreditCard, FiTrash2 } from "react-icons/fi";
+import CardWithHeader from "../ui/card-with-header";
+import { useRouter } from "next/navigation";
+
+const CartItems = () => {
+    const { push } = useRouter();
+
+    const totalPrice = cartList.reduce((total, item) => total + item.price * item.qty, 0)
+
+    const payment = () => {};
+    
+    return (
+        <CardWithHeader title="Card Items">
+            <div className="overflow-auto max-h-75">
+                {
+                    cartList.map((item, index) => (
+                        <div className="border-b border-gray-200 px-4 flex gap-3" key={index}>
+                            <div className="bg-primary-light aspect-square w-16 flex justify-center items-center">
+                                <Image src={`/images/products/${item.imgUrl}`} width={63} height={63} alt={item.name} />
+                            </div>
+                            <div className="self-center">
+                                <div className="text-sm font-medium">{item.name}</div>
+                                <div className="flex gap-3">
+                                    <div>{item.qty}</div>
+                                    <div className="text-primary">{priceFormatter(item.price)}</div>
+                                </div>
+                            </div>
+                            <Button size="small" variant="ghost" className="w-7 h7 p-0! self-center ml-auto">
+                                <FiTrash2 />
+                            </Button>
+                        </div>       
+                    ))
+                }
+            </div>
+            <div className="border-t border-gray-200 p-4">
+                <div className="flex justify-between font-semibold">
+                    <div className="text-sm">Total</div>
+                    <div className="text-primary text-xs">{priceFormatter(totalPrice)}</div>
+                </div>
+                <Button variant="dark" className="w-full mt-4" onClick={() => push("/payment")}>
+                    <FiCreditCard /> Procced to Payment 
+                </Button>
+            </div>
+        </CardWithHeader>
+        
+    )
+}
+
+export default CartItems;
---
+++ app/(landing)/components/product-detail/product-action.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import Button from "../ui/button";
+import { FiArrowRight, FiShoppingBag, FiChevronUp, FiChevronDown } from "react-icons/fi";
+import { useRouter } from "next/router";
+
+const ProductAction = () => {
+    const { push } = useRouter();
+    const [qty, setQty] = useState(1)
+    const checkout = () => {};
+    
+    return (
+        <div className="flex gap-5">
+            <div className="border border-gray-500 inline-flex w-fit min-w-20.5">
+                <div className="aspect-square text xl font-medium border-r border-gray-500 flex justify-center items-center">
+                    <span>{qty}</span>
+                </div>
+                <div className="flex flex-col">
+                    <button className="border-b border-gray-500 curson-pointer h-1/2 aspect-square flex justify-center items-center" onClick={() => setQty(qty+1)}>
+                        <FiChevronUp />
+                    </button>
+                    <button className="border-b border-gray-500 curson-pointer h-1/2 aspect-square flex justify-center items-center" onClick={() => setQty(qty > 1 ? qty - 1 : qty)}>
+                        <FiChevronDown />
+                    </button>
+                </div>
+            </div>
+            <Button className="px-20 w-full">
+                <FiShoppingBag size={24}/>
+                Add to Cart
+            </Button>
+            <Button variant="dark" className="px-20 w-full" onClick={() => push("/checkout")}>
+                Checkout Now
+                <FiArrowRight size={24}/>
+            </Button>
+        </div>
+    )
+}
+
+export default ProductAction;
