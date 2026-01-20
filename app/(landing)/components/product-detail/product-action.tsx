00c7e0d1b3a68a0674a0abe10d2e67f6112effd6
matiusfebrianchristo <chriss.tkj@gmail.com>
2026-01-20 16:45:18 +0700

    Update for product detail

--- app/(landing)/components/product-detail/product-action.tsx
+++ app/(landing)/components/product-detail/product-action.tsx
@@ -5,7 +5,11 @@ import Button from "../ui/button";
 import { FiArrowRight, FiShoppingBag, FiChevronUp, FiChevronDown } from "react-icons/fi";
 import { useRouter } from "next/navigation";
 
-const ProductAction = () => {
+type TProductActionProps = {
+    stock: number
+}
+
+const ProductAction = ({stock}:TProductActionProps) => {
     const { push } = useRouter();
     const [qty, setQty] = useState(1)
     const checkout = () => {};
@@ -17,7 +21,7 @@ const ProductAction = () => {
                     <span>{qty}</span>
                 </div>
                 <div className="flex flex-col">
-                    <button className="border-b border-gray-500 curson-pointer h-1/2 aspect-square flex justify-center items-center" onClick={() => setQty(qty+1)}>
+                    <button className="border-b border-gray-500 curson-pointer h-1/2 aspect-square flex justify-center items-center" onClick={() => setQty(qty < stock ? qty+1 : qty)}>
                         <FiChevronUp />
                     </button>
                     <button className="border-b border-gray-500 curson-pointer h-1/2 aspect-square flex justify-center items-center" onClick={() => setQty(qty > 1 ? qty - 1 : qty)}>
